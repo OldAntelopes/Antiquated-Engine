399df3993bbbfa563bb41f83bc8cd130c191d019
Mitchell Goodwin <mitch.virtual@gmail.com>
2022-08-09 13:15:42 +0100

    Added obfuscation header

---
+++ Pub/Include/UnivObfuscation.h
@@ -0,0 +1,33 @@
+#ifndef SECURITY_LIB
+#define SECURITY_LIB
+
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#define MAX_CHARS_IN_PASSWORD		24
+
+extern void		EncryptPassword( char* pcPassword );
+extern void		DecryptPassword( char* pcPassword );
+
+extern ulong	GetUniqueID ( void );
+extern int		SetUniqueID ( int nIDVal );
+extern int		LoadUniqueID ( void );
+
+extern BOOL		SecureStrings ( char* pcComString, ushort uwBillingID, short wWhoFrom );
+extern BOOL		CheckRegistration ( int nServerID, char* szPassword, char* szOwner );
+
+extern BOOL		RegisteredIslandInitCheck( char* szIslandName, ulong ulUID, char* szRegisteredError );
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+
+
+
+#endif
